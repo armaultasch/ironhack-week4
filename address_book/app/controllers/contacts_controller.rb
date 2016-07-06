b0741f19601ec198f2d245ee382dbfd882abe02e
Alison Maultasch <armaultasch@gmail.com>
2016-07-06 10:31:54 -0400

    added id sites to projects

--- address_book/app/controllers/contacts_controller.rb
+++ address_book/app/controllers/contacts_controller.rb
@@ -1,6 +1,7 @@
 class ContactsController < ApplicationController
 	def index
 		@contacts_array = Contact.order(name: "asc")
+
 		render "index"
 	end
 
@@ -16,9 +17,15 @@ class ContactsController < ApplicationController
       :phone => params[:contact][:phone],
       :email => params[:contact][:email])
 
+	# email_pattern = /^\W+@\w+\.[A-Za-z]+$/
+	# phone_pattern = /(\+1)?\(?[0-9]{3}\)? ?-?[0-9]{3}-?[0-9]{4}/
+	# if (contact.email =~ email_pattern) != nil && 
+	# 	(contact.phone =~ phone_pattern) != nil
 	contact.save
     # render(:text => contact.attributes)
     redirect_to '/contacts'
+# else redirect_to 'contacts/new'
+# end
 	end	
 
 	def show
