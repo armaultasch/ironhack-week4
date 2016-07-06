5dfe450c5c5b4d1c0a0579cadb41663a9ee4224b
Alison Maultasch <armaultasch@gmail.com>
2016-07-06 12:04:15 -0400

    time entries table linked to projects

--- timetracking/app/controllers/projects_controller.rb
+++ timetracking/app/controllers/projects_controller.rb
@@ -10,4 +10,19 @@ class ProjectsController < ApplicationController
 		@my_project = Project.find(params[:id])
 		render "show"
 	end
+
+	def new
+		@my_project = Project.new
+		render "new"
+	end
+
+	def create
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+
+		redirect_to "/projects"
+		
+	end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 	get '/', to: 'site#home'
 	get '/contact', to: 'site#contact'
 	get '/projects', to: 'projects#index'
+	get '/projects/new', to: 'projects#new'
+	post '/projects/new', to: 'projects#new'
+	post '/projects', to: 'projects#create'
 	get '/projects/:id', to: 'projects#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
