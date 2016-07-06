d6f7f07aa743afab9505a27017a64b07cf363a7c
Alison Maultasch <armaultasch@gmail.com>
2016-07-06 14:56:34 -0400

    adding another controller to display new task

--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 	post '/projects/new', to: 'projects#new'
 	post '/projects', to: 'projects#create'
 	get '/projects/:id', to: 'projects#show'
+	get '/projects/:project_id/time_entries', to: 'time_entries#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
