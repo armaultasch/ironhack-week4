a578f5d2529235d3b722840420aecde07007c71c
Alison Maultasch <armaultasch@gmail.com>
2016-07-07 14:18:06 -0400

    partials and eliminating redundancy

--- timetracking/app/controllers/time_entries_controller.rb
+++ timetracking/app/controllers/time_entries_controller.rb
@@ -13,16 +13,36 @@ class TimeEntriesController < ApplicationController
 
 	def create
 		@my_project = Project.find(params[:project_id])
-		@my_entry = @my_project.time_entries.new(
-			:hours => params[:time_entry][:hours],
-			:minutes => params[:time_entry][:minutes],
-			:date => params[:time_entry][:date],
-			:comments => params[:time_entry][:comments])
+		@my_entries = @my_project.time_entries.new(entry_params)
 
-		if @my_entry.save
+		if @my_entries.save
 			redirect_to "/projects/#{@my_project.id}/time_entries"
 		else render "new"
 		end
 	end
+	def edit
+		@my_project = Project.find params[:project_id]
+		@my_entries = @my_project.time_entries.find params[:id]
+		render "edit"
+	end
+
+	def update
+		@my_project = Project.find_by(id: params[:project_id])
+		@my_entries = @my_project.time_entries.find_by(id: params[:id])
+
+		if @my_entries.update(entry_params)
+		redirect_to action: "index", controller: "time_entries",
+		project_id: @my_project.id
+	else
+		render "edit"
+	end
+end
+
+private
+
+def entry_params
+	params.require(:time_entry).permit(:hours, :minutes, :date, :comments)
+end
+
 
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	get '/', to: 'site#home'
-	get '/contact', to: 'site#contact'
+	# get '/', to: 'site#home'
+	# get '/contact', to: 'site#contact'
 	get '/projects', to: 'projects#index'
 	get '/projects/new', to: 'projects#new'
 	post '/projects/new', to: 'projects#new'
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
 	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
 	post '/projects/:project_id/time_entries',
 		to: 'time_entries#create', as: :project_time_entries
+	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
+  	patch 'projects/:project_id/time_entries/:id', to: 'time_entries#update', as: "project_time_entry"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
