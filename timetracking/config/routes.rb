2d22c9c132b88f028b5680234ecf4d50f4562c37
Alison Maultasch <armaultasch@gmail.com>
2016-07-07 15:19:21 -0400

    resources added to remove routes

--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-	# get '/', to: 'site#home'
-	# get '/contact', to: 'site#contact'
-	get '/projects', to: 'projects#index'
-	get '/projects/new', to: 'projects#new'
-	post '/projects/new', to: 'projects#new'
-	post '/projects', to: 'projects#create'
-	get '/projects/:id', to: 'projects#show'
-	get '/projects/:project_id/time_entries', to: 'time_entries#index'
-	get '/projects/:project_id/time_entries/new', to: 'time_entries#new'
-	post '/projects/:project_id/time_entries',
-		to: 'time_entries#create', as: :project_time_entries
-	get '/projects/:project_id/time_entries/:id/edit', to: 'time_entries#edit'
-  	patch 'projects/:project_id/time_entries/:id', to: 'time_entries#update', as: "project_time_entry"
-  	delete 'projects/:project_id/time_entries/:id', to: "time_entries#destroy"
+	get '/', to: 'site#home'
+	get '/contact', to: 'site#contact'
+	resources :projects, only: [:index, :show, :new, :create] do
+		resources :time_entries, except: [:show]
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
