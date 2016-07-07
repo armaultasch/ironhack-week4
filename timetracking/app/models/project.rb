52a2b42763f8c065c21acfcf4f1e4aea177beb9a
Alison Maultasch <armaultasch@gmail.com>
2016-07-07 10:53:59 -0400

    deleting things

--- timetracking/app/models/project.rb
+++ timetracking/app/models/project.rb
@@ -1,6 +1,20 @@
 class Project < ApplicationRecord
 	has_many :time_entries
+
+	def self.clean_old
+		#find projects older than a week
+		bad_project = Project.where('created_at < ?', 1.week)
+		#"created_at < ? AND created_at < ?", 1.week.ago
+		#destroy them
+		bad_project.destroy_all
+	end
 end
+
+
+#in the rails console try:
+# Project.clean_old
+
+
 #the_project = Project.find_by(id: 1)
 #the_project.time_entries
 
